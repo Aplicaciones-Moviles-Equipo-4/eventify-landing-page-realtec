ebb759f7a5dc5f15ed2e5ea66279add66402eeb6
Matias Armestar <102616485+MatiasArmestar@users.noreply.github.com>
2026-07-01 01:05:38 -0500

    feat: add index.html, assets directory & script.js

---
+++ script.js
@@ -0,0 +1,147 @@
+const translations = {
+    es: {
+
+        "header.home": "Inicio",
+        "header.about.the.product": "Acerca del producto",
+        "header.functionalities": "Funcionalidades",
+        "header.benefits": "Beneficios",
+        "header.plans": "Planes",
+        "header.about.the.team": "Acerca del Equipo",
+        "header.about.the.product": "Acerca del Producto",
+        "header.register": "Regístrate",
+        "hero.slogan": "Conecta - Organiza - Celebra",
+        "hero.description": "Una plataforma para que anfitriones encuentren al organizador ideal, y organizadores hagan crecer su negocio",
+        "hero.find.your.organizer": "Encuentra a tu organizador",
+        "hero.join.as.organizer": "Únete como organizador",
+        "about.the.product.slogan": "Aprende como funciona Eventify",
+        "about.the.product.description": "Mira nuestro video tutorial y aprende a sacarle el máximo provecho desde el primer evento",
+        "about.the.product.phrase.1": "Gestiona eventos con facilidad",
+        "about.the.product.phrase.2": "Organiza tus eventos sin estrés",
+        "functionalities.slogan": "¿Qué te ofrece Eventify?",
+        "functionalities.organizers": "Organizadores",
+        "functionalities.organizers.phrase.1": "Crea eventos y cotizaciones",
+        "functionalities.organizers.phrase.2": "Planificación de tareas por evento",
+        "functionalities.organizers.phrase.3": "Calendario interactivo para organizar eventos",
+        "functionalities.hosts": "Anfitriones",
+        "functionalities.hosts.phrase.1": "Búsqueda y filtrado de organizadores",
+        "functionalities.hosts.phrase.2": "Comunicación directa con el organizador",
+        "functionalities.hosts.phrase.3": "Seguimiento de la planificación del organizador",
+        "benefits.slogan" : "¿Porqué utilizar Eventify?",
+        "benefits.organizers" : "Organizadores",
+        "benefits.organizers.phrase.1" : "Conecta con más clientes cada semana",
+        "benefits.organizers.phrase.2" : "Gestiona tus eventos en un solo lugar",
+        "benefits.organizers.phrase.3" : "Muestra tu experiencia y genera confianza",
+        "benefits.hosts" : "Anfitriones",
+        "benefits.hosts.phrase.1" : "Encuentra organizadores profesionales",
+        "benefits.hosts.phrase.2" : "Ahorra tiempo y esfuerzo",
+        "benefits.hosts.phrase.3" : "Coordina todo desde un solo lugar",
+        "plans.option.frecuency" : "(Mensual)",
+        "plans.option.button" : "Comenzar ahora",
+        "plans.title" : "¿Eres organizador de eventos?",
+        "plans.description" : "Conoce nuestros planes que potenciarán tu labor como organizador de eventos",
+        "plans.option.1.title" : "Plan Básico",
+        "plans.option.1.description" : "Ideal para quienes estan comenzando. Gestiona tus primero eventos y explora todas las herramientas",
+        "plans.option.1.detail" : "Permite trabajar hasta con 2 anfitriones al mismo tiempo",
+        "plans.option.2.title" : "Plan Estándar",
+        "plans.option.2.description" : "Perfecto para organizadores en crecimiento. Mantén tu flujo de trabajo eficiente con más capacidad de atención y organización",
+        "plans.option.2.detail" : "Permite trabajar hasta con 5 anfitriones al mismo tiempo",
+        "plans.option.3.title" : "Plan Premium",
+        "plans.option.3.description" : "Pensado para profesionales consolidados. Coordina varios eventos a la vez y maximiza tu capacidad operativa sin límites",
+        "plans.option.3.detail" : "Permite trabajar hasta con 10 anfitriones al mismo tiempo",
+        "about.the.team.slogan" : "Conoce a Realtec, el equipo encargado en diseñar y desarrollar Eventify",
+        "team.profile.profession" : "Ingeniero de Software",
+        "footer.contact" : "Contacto",
+        "footer.social.networks" : "Redes sociales",
+        "footer.copyright" : "Copyright © 2025 Eventify. Todos los derechos reservados.",
+    },
+
+    en: {
+
+        "header.home": "Home",
+        "header.about.the.product": "About the product",
+        "header.functionalities": "Functionalities",
+        "header.benefits": "Benefits",
+        "header.plans": "Plans",
+        "header.about.the.team": "About the Team",
+        "header.about.the.product": "About the Product",
+        "header.register": "Register",
+        "hero.slogan": "Connect - Organize - Celebrate",
+        "hero.description": "A platform for hosts to find the ideal organizer, and organizers to grow their business",
+        "hero.find.your.organizer": "Find your organizer",
+        "hero.join.as.organizer": "Join as an organizer",
+        "about.the.product.slogan": "Learn how Eventify works",
+        "about.the.product.description": "Watch our video tutorial and learn how to get the most out of it from the first event",
+        "about.the.product.phrase.1": "Manage events with ease",
+        "about.the.product.phrase.2": "Organize your events without stress",
+        "functionalities.slogan": "What does Eventify offer you?",
+        "functionalities.organizers": "Organizers",
+        "functionalities.organizers.phrase.1": "Create events and quotes",
+        "functionalities.organizers.phrase.2": "Task planning by event",
+        "functionalities.organizers.phrase.3": "Interactive calendar to organize events",
+        "functionalities.hosts": "Hosts",
+        "functionalities.hosts.phrase.1": "Search and filter organizers",
+        "functionalities.hosts.phrase.2": "Direct communication with the organizer",
+        "functionalities.hosts.phrase.3": "Monitoring the organizer's planning",
+        "benefits.slogan" : "Why use Eventify?",
+        "benefits.organizers" : "Organizers",
+        "benefits.organizers.phrase.1" : "Connect with more clients every week",
+        "benefits.organizers.phrase.2" : "Manage your events in one place",
+        "benefits.organizers.phrase.3" : "Show your experience and build trust",
+        "benefits.hosts" : "Hosts",
+        "benefits.hosts.phrase.1" : "Find professional organizers",
+        "benefits.hosts.phrase.2" : "Save time and effort",
+        "benefits.hosts.phrase.3" : "Coordinate everything from one place",
+        "plans.option.frecuency" : "(Monthly)",
+        "plans.option.button" : "Start now",
+        "plans.title" : "Are you an event organizer?",
+        "plans.description" : "Learn about our plans that will enhance your work as an event organizer",
+        "plans.option.1.title" : "Basic Plan",
+        "plans.option.1.description" : "Ideal for those starting out. Manage your first events and explore all the tools",
+        "plans.option.1.detail" : "Allows you to work with up to 2 hosts at the same time",
+        "plans.option.2.title" : "Standard Plan",
+        "plans.option.2.description" : "Perfect for growing organizers. Keep your workflow efficient with more attention and organization",
+        "plans.option.2.detail" : "Allows you to work with up to 5 hosts at the same time",
+        "plans.option.3.title" : "Premium Plan",
+        "plans.option.3.description" : "Designed for established professionals. Coordinate several events at the same time and maximize your operational capacity without limits",
+        "plans.option.3.detail" : "Allows you to work with up to 10 hosts at the same time",
+        "about.the.team.slogan" : "Meet Realtec, the team in charge of designing and developing Eventify",
+        "team.profile.profession" : "Software Engineer",
+        "footer.contact" : "Contact",
+        "footer.social.networks" : "Social networks",
+        "footer.copyright" : "Copyright © 2025 Eventify. All rights reserved.",
+    }
+};
+
+let currentLanguage = "es";
+
+const toggleButton = document.getElementById("language-toggle");
+
+function changeLanguage(language){
+
+    document.documentElement.lang = language;
+
+    document.querySelectorAll("[data-i18n]").forEach(element => {
+
+        const key = element.getAttribute("data-i18n");
+
+        element.textContent = translations[language][key];
+
+    });
+
+}
+
+toggleButton.addEventListener("click", () => {
+
+    currentLanguage = currentLanguage === "es" ? "en" : "es";
+
+    changeLanguage(currentLanguage);
+
+    toggleButton.textContent =
+        currentLanguage === "es"
+        ? "🇪🇸 ES"
+        : "🇺🇸 EN";
+
+});
+
+// Idioma inicial
+changeLanguage(currentLanguage);
